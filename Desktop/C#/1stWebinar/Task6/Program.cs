5b1d59cb0eeb463d392d530fac61e40f9c6f5577
Ivan <mmavrin80@gmail.com>
2022-03-15 19:48:08 +0300

    Сделал 6 таск

---
+++ Desktop/C#/1stWebinar/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// задача на вывод всех целых чисел в промежутке
+// перевести в стринг и записать через запятую
+Console.Write("Введите число: ");
+string str_N = Console.ReadLine();
+int N = int.Parse(str_N);
+
+int N1 = -N;
+string ans = " ";
+
+while (N1 <= N){
+    ans = ans + $"{N1}, ";
+    N1 = N1 + 1;
+}
+Console.WriteLine($"Ответ: {ans}");
