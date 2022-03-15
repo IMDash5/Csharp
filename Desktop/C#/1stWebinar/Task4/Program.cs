6b1f82d0365bb5c33b12e2cc0b9f035fc1a5f329
Ivan <mmavrin80@gmail.com>
2022-03-15 18:55:37 +0300

    Задача 4

---
+++ Desktop/C#/1stWebinar/Task4/Program.cs
@@ -0,0 +1,7 @@
+﻿// Программа вычисления функции x = f(a)
+Console.Write("Введите число а: ");
+string str_a = Console.ReadLine();
+int a = int.Parse(str_a);
+
+int x = a * 3;
+Console.WriteLine($"Функция x = f(a) равна => {x}");
