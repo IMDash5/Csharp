b376d4b7f9588b2139c87f817f4322485e515ebe
Ivan <mmavrin80@gmail.com>
2022-03-22 21:13:08 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,50 @@
+﻿int Prompt(string message)
+{
+    Console.WriteLine(message);
+    string StrCord = Console.ReadLine();
+    int cord = int.Parse(StrCord);
+    return cord;
+}
+
+int x = Prompt("Введите х: ");
+int y = Prompt("Введите y: ");
+
+int quarter = 0;
+int answer = 0;
+
+int Quart(int cordX, int cordY)
+{
+    if (cordX > 0)
+    {
+        if (cordY > 0)
+        {
+            quarter = 1;
+        }
+        else 
+        {
+            quarter = 4;
+        }
+    }
+    else 
+    {
+        if (cordY > 0)
+        {
+            quarter = 2;
+        }
+        else 
+        {
+            quarter = 3;
+        }
+    }
+    return quarter;
+}
+
+if (x == 0 || y ==0)
+{
+    Console.WriteLine("Одна из координат равна нулю");
+}
+else 
+{
+    answer = Quart(x, y);
+    Console.WriteLine(answer);
+}
